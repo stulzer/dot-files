aa4ccd7558f2247aaab86505ead5848633fcc2f1
Rubens Stulzer <rubens.stulzer@gmail.com>
2025-01-15 11:58:10 +0100

    feat: update plugin versions and remove avante.nvim
    - Updated various plugin versions in lazy-lock.json
    - Removed avante.nvim plugin from init.lua

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -211,49 +211,6 @@ return {
   },
 
   {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      provider = "copilot",
-      copilot = {
-        model = "claude-3.5-sonnet",
-      },
-    },
-    build = "make",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "zbirenbaum/copilot.lua",
-      {
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
-
-  {
     "folke/noice.nvim",
     event = "VeryLazy",
     opts = {
@@ -378,20 +335,6 @@ return {
             format = { "{message}" },
             win_options = { concealcursor = "n", conceallevel = 3 },
           },
-        },
-      },
-      markdown = {
-        hover = {
-          ["|(%S-)|"] = vim.cmd.help,
-          ["%[.-%]%((%S-)%)"] = require("noice.util").open,
-        },
-        highlights = {
-          ["|%S-|"] = "@text.reference",
-          ["@%S+"] = "@parameter",
-          ["^%s*(Parameters:)"] = "@text.title",
-          ["^%s*(Return:)"] = "@text.title",
-          ["^%s*(See also:)"] = "@text.title",
-          ["{%S-}"] = "@parameter",
         },
       },
       health = {
