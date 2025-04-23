aa9e3786df4f2a755d8c48fb33088d6ca79b5e65
Rubens Stulzer <rubens.stulzer@gmail.com>
2025-04-23 21:03:55 +0200

    Update runtime versions and configs
    * Update Ruby from 3.3.5 to 3.4.1 and Node.js from 22.0.0 to 22.14.0
    * Clean up duplicate entries in gitignore
    * Switch kitty terminal font to Maple Mono
    * Add avante.nvim with support packages for AI assistance
    * Update various nvim plugin dependencies

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -210,4 +210,52 @@ return {
       },
     },
   },
+
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      provider = "copilot",
+      copilot = {
+        model = "claude-3.5-sonnet",
+      },
+    },
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
 }
